6a46cf6535de490d76382669d5dc040aa02a7578
Leon de Klerk <developer@leondeklerk.com>
2023-01-02 23:19:22 +0100

    Add implementation
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # Tasmota MQTT stats aggregator
 
-Runs a daily function to retrieve statistics for configured Tasmota MQTT devices and store these.
-Mainly intended to gain insight in daily energy usage.
+Runs a daily function to retrieve statistics for configured Tasmota MQTT devices and stores these.
+Mainly intended to gain insight into daily energy usage, but does not have any data processing/graphing yet.
+
+It pulls the data from all configured devices at midnight every day. The data is written to files in CSV format `date,usage` where both values point to the day before (e.g. on 1/1/2023 it gets the data of 31/12/2022). After a response has been received for each configured topic, the client will disconnect until the next polling moment.
+
+## Environment variables
+
+All environment variables are optional and can be left out. By default, the connection is made to `localhost:1883`.
+
+The following environment variables can be configured:
+
+```env
+MQTT_PASSWORD="admin"
+MQTT_USERNAME="admin"
+MQTT_HOST="localhost"
+MQTT_PORT="1883"
+# File name of the CA file that self-signed the broker cert in pem format.
+MQTT_CA_FILE="ca.pem"
+MQTT_PROTOCOL="mqtts"
+# comma-separated string of device topics.
+MQTT_TOPICS="device1,device2"
+MQTT_TZ="Europe/Amsterdam"
+```
+
+## File structure
+
+Each device defined via the `MQTT_TOPICS` variable will create its own file where data is stored. For example, a device with the topic `device1` will create a `device1` file. If a response is received containing a topic that is not configured, it will be placed in a file called `fallback`. The CSV format of this file is `date,topic,usage`.
+
+The files are stored in the `data` folder (`/app/data` binding with Docker).
+The optional CA file must be placed in the `config` folder (`/app/config` binding with Docker)
---
+++ src/index.ts
@@ -0,0 +1,129 @@
+import * as mqtt from "mqtt";
+import fs from "fs";
+import * as dotenv from "dotenv";
+import { CronJob } from "cron";
+dotenv.config();
+
+let client: mqtt.MqttClient | null = null;
+let pending: string[] = [];
+
+new CronJob(
+	"00 00 00 * * *",
+	() => {
+		pending = [];
+		if (client !== null) {
+			client.end(undefined, undefined, () => {
+				client = run();
+			});
+		} else {
+			client = run();
+		}
+	},
+	null,
+	true,
+	process.env?.MQTT_TZ || "Europe/Amsterdam"
+);
+
+function run() {
+	// Create the connection options
+	const options = {
+		rejectUnauthorized: true,
+	} as mqtt.IClientOptions;
+
+	options.host = process.env?.MQTT_HOST || "localhost";
+	options.port = parseInt(process.env?.MQTT_PORT || "1883");
+
+	// Ignore as we don't do any checks on the protocol value.
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	options.protocol = process.env?.MQTT_PROTOCOL;
+
+	if (process.env?.MQTT_CA_FILE) {
+		const CA = fs.readFileSync(`../config/${process.env.MQTT_CA_FILE}`);
+		options.ca = CA;
+	}
+
+	options.username = process.env?.MQTT_USERNAME;
+	options.password = process.env?.MQTT_PASSWORD;
+
+	const client = mqtt.connect(options);
+
+	// Get the device topics
+	const topicString = process.env?.MQTT_TOPICS || "";
+	const topics = topicString.split(",");
+
+	client.on("connect", () => {
+		for (const topic of topics) {
+			pending.push(topic);
+			client.subscribe(`stat/${topic}/RESULT`);
+			client.publish(`cmnd/${topic}/EnergyYesterday`, "");
+		}
+	});
+
+	client.on("message", onMessage);
+
+	client.on("error", (error) => {
+		if (error) {
+			console.log("Client error", error);
+		}
+	});
+	return client;
+}
+
+function onMessage(topic: string, message: Buffer) {
+	const parts = topic.split("/");
+	// Only parse statistic messages
+	if (parts[0] === "stat") {
+		// Find the correct device
+		const device = parts[1] || "fallback";
+		const data = JSON.parse(message.toString());
+
+		// Get the necessary device data
+		if (data["EnergyYesterday"] && data["EnergyYesterday"]["Yesterday"]) {
+			// Actual usage of the previous day
+			const usage = data["EnergyYesterday"]["Yesterday"];
+
+			// Format date to string: YYYY-MM-DD
+			const date = new Date();
+			date.setDate(date.getDate() - 1);
+			const month = (date.getMonth() + 1).toString().padStart(2, "0");
+			const day = date.getDate().toString().padStart(2, "0");
+			const dateString = `${date.getFullYear()}-${month}-${day}`;
+
+			if (device === "fallback") {
+				writeToFile(device, `${dateString},${topic},${usage}`);
+			} else {
+				writeToFile(device, `${dateString},${usage}`);
+			}
+		}
+		pending = pending.filter((value) => value !== device);
+	}
+	client?.unsubscribe(topic);
+	if (pending.length === 0) {
+		client?.end();
+		client = null;
+	}
+}
+
+function writeToFile(fileName: string, content: string) {
+	fs.access(`../data/${fileName}`, fs.constants.F_OK, (err) => {
+		if (err) {
+			let header = "date,usage";
+			if (fileName === "fallback") {
+				header = "date,topic,usage";
+			}
+
+			fs.writeFile(`../data/${fileName}`, `${header}\n${content}\n`, (err) => {
+				if (err) {
+					console.log("Write error:", err);
+				}
+			});
+		} else {
+			fs.appendFile(`../data/${fileName}`, `${content}\n`, (err) => {
+				if (err) {
+					console.log("Append error:", err);
+				}
+			});
+		}
+	});
+}
